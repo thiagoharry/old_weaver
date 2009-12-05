1ef665d388db0c475e0ff402d5bbacd46c1c017f
thiagoharry <thiagoharry>
2009-12-05 16:24:31 +0000

    The example of the bouncing ball was apprimorated.

--- examples/ball/src/game.c
+++ examples/ball/src/game.c
@@ -27,37 +27,37 @@ int main(int argc, char **argv){
   struct surface *ball;
 
   awake_the_weaver(); // Initializing Weaver API
-  hide_cursor();
+  hide_cursor(); // Makes the cursos disappear
 
-  ball = new_image("ball.png");
-  dx = dy = 2;
-  circle_x = window_width / 2;
+  ball = new_image("ball.png"); // Load a new PNG image
+  dx = dy = 1;                  // The ball moves 1 pixel per frame
+  circle_x = window_width / 2;  // The center of the ball coordinates
   circle_y = window_height / 2;
 
   // Main loop
   for(;;){
     get_input();
-    if(keyboard[ESC]){
-      break;
+    if(keyboard[ANY]){
+      break; // If the user press any key, the game halts
     }
 
-    fill_rectangle(circle_x - RADIUS, circle_y - RADIUS, 2*RADIUS, 2*RADIUS, BLACK);
-
-    circle_x += dx;
+    fill_circle(circle_x, circle_y , RADIUS + 5, BLACK); // Erasing ball
+   
+    circle_x += dx; // Moving ball
     circle_y += dy;
 
-    if(circle_y < RADIUS || circle_y > window_height - RADIUS){
-      dy *= -1;
+    if(circle_y < RADIUS || circle_y > window_height - RADIUS){ 
+      dy *= -1; // If it hits the top or bottom of screen
       play_soundfile("hit.ogg");
     }
     if(circle_x < RADIUS || circle_x > window_width - RADIUS){
-      dx *= -1;
+      dx *= -1; // If it hits the left or right side of the screen
       play_soundfile("hit.ogg");
     }
-
+    // Drawing the ball
     blit_surface(ball, window, 0, 0, 2 * RADIUS, 2 * RADIUS, circle_x - RADIUS, circle_y - RADIUS);    
 
-    weaver_rest(10000000);
+    weaver_rest(5000000); // Waiting 0.05 seconds...
   }
   may_the_weaver_sleep();
   return 0;
