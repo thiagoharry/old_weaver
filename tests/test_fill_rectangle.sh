2bf5ccaefc795e4a12c9a986e832295f2f31aaf1
Thiago "Harry" Leucz Astrizi <thiagoharry@riseup.net>
2012-07-29 00:24:46 -0300

    Added test for 'fill_rectangle' function.

---
+++ tests/test_fill_rectangle.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# The source to be tested
+cd tests/test_project
+echo -ne "#include \"weaver/weaver.h\"\n
+#include \"game.h\"\n
+int main(int argc, char **argv){
+  awake_the_weaver(); // Initializing Weaver API  
+  XSync(_dpy, 1); 
+  DEBUG_TIMER_START();
+  fill_rectangle(0, 0, window_width, window_height, BLUE);
+  XSync(_dpy, 1);
+  DEBUG_TIMER_END();
+  may_the_weaver_sleep();
+  return 0;\n
+}" > src/game.c
+make &> /dev/null
+j=0
+sum=0
+echo -n "" > data.txt 
+while (( $j < 100 )); do
+    echo ${j}"%"
+    media=$(./test_project $j)
+    echo -n ${j}" " >> data.txt
+    echo ${media} >> data.txt
+    sum=$((${sum}+${media}))
+    j=$(($j+1))
+done
+sum=$((${sum}/100))
+
+echo "set output \"fill_rectangle.eps\"" > ../gnuplot_instructions.txt
+echo "set terminal postscript eps enhanced;" >> ../gnuplot_instructions.txt
+echo "plot \"data.txt\" with lines;" >> ../gnuplot_instructions.txt
+
+echo "\subsection{fill\_rectangle(a,b,c,d,e)}" >> ../tex/report.tex
+echo "Function used for drawing and filling a rectangle in the" >> ../tex/report.tex
+echo "screen, given it's coordinates. " >> ../tex/report.tex
+gnuplot ../gnuplot_instructions.txt
+mv fill_rectangle.eps ../tex
+
+echo "" >> ../tex/report.tex
+echo "\includegraphics{tests/tex/fill_rectangle.eps}" >> ../tex/report.tex
+echo "" >> ../tex/report.tex
+echo "As this function has a constant theoretical" >> ../tex/report.tex
+echo "complexity, the time, in nanosseconds is " >> ../tex/report.tex
+echo "approximated by the function \$f(x)=${sum}\$." >> ../tex/report.tex
+cd -
