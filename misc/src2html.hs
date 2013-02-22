fad29e4b2ed3a5470695c155d3e97c2603d7fc6a
Thiago "Harry" Leucz Astrizi <harry@bitbitbit.com.br>
2013-02-21 22:46:40 -0300

    Added copyright and license text in the auxiliary program misc/src2html.hs.

--- misc/src2html.hs
+++ misc/src2html.hs
@@ -1,5 +1,25 @@
 {-# LANGUAGE Haskell2010 #-}
 
+-- Copyright (C) 2012-2013 Thiago Leucz Astrizi
+
+-- This is the source code of src2html, an auxiliary program used to
+-- help in the creation of html documentation of C source code. It was
+-- created to help the documentation of the Weaver Framework.
+
+-- src2html is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- src2html is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with src2html.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import System.IO
 import Data.List
 import Data.Maybe
